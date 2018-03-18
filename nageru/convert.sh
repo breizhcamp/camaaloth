f4e564392c82063f42bb9976283ce448b89035ce
Yoann Dubreuil <yoann.dubreuil@gmail.com>
2018-03-18 18:48:36 +0100

    better not remove NUT files, we never know....

--- nageru/convert.sh
+++ nageru/convert.sh
@@ -4,5 +4,5 @@ for i in /home/capture2/Vidéos/*nut; do
   cat $i | ffmpeg -i - -vcodec copy $(echo ${i%%.nut}|sed 's/[:+]//g').mp4
 done
 
-rm /home/capture2/Vidéos/*nut
-
+mkdir -p /home/capture2/Vidéos/old
+mv /home/capture2/Vidéos/*nut /home/capture2/Vidéos/old
