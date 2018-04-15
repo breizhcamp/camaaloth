efa248f6fe9c1939a1e847968a6433cf284adcd0
Yoann Dubreuil <yoann.dubreuil@gmail.com>
2018-04-15 21:03:37 +0200

    automate cut to make it accurate
    -ss doesn't work with copy codec unless it is given a keyframe

--- nageru/merge-intro.sh
+++ nageru/merge-intro.sh
@@ -8,6 +8,38 @@ for i in "$@";  do
         -acodec copy -c:v libx264 -preset veryslow -crf 10 \
         intro.ts
     fi
+
+    source="$(find . -type f -name 'record*.mkv' | head -n 1)"
+    start_time="$(cat start_time)" # HH:MM:SS seek time notation in FFmpeg
+    echo "$start_time" | grep -q "^[0-9][0-9]:[0-9][0-9]:[0-9][0-9]$"
+    valid_ts=$?
+    if [[ ! valid_ts ]]; then
+        echo "Invalid timestamp in start_time file"
+    fi 
+    if [[ ! -r video.ts && -r start_time && -r $source && valid_ts ]]; then
+        seek_arg="00:00:00"
+        start_time_in_sec="00"
+        if [[ $start_time != "00:00:00" ]]; then
+            # We look for the keyframe just before the start time to accurately cut the source file and use copy codec
+            # It is not pretty but there's no really any other solution
+            start_time_in_sec="$(echo "$start_time"| awk -F: '{ print ($1 * 3600) + ($2 * 60) + $3 }')"
+            seek_ts=$(ffprobe -loglevel error -skip_frame nokey -select_streams v:0 -show_entries frame=pkt_pts_time -of csv=print_section=0 "$source" | \
+            while read keyframe_ts; do
+                keyframe_in_sec="${keyframe_ts%.*}"
+                if [[ $start_time_in_sec -gt $keyframe_in_sec ]]; then
+                    previous_ts="$keyframe_ts"
+                else
+                    echo $previous_ts
+                    break # stop reading from the pipe to kill ffprobe
+                fi
+            done)
+        fi
+        seek_sec="${seek_ts%.*}"
+        seek_arg="$(($seek_sec / 3600)):$(($seek_sec / 60)):$(($seek_sec % 60)).${seek_ts#*.}"
+        echo $seek_arg
+        ffmpeg -y -i "$source" -ss "$seek_arg" -ab 192k -af "afade=t=in:st=$start_time_in_sec:d=3" -acodec aac -vcodec copy video.ts
+    fi
+
     if [[ ! -r final.mp4 && -r video.ts ]]; then
     ffmpeg -y -i "concat:intro.ts|video.ts" \
         -acodec copy -c:v copy \
