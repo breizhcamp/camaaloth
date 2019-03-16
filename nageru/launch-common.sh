b3bd22cfc3eef3d6e9ac13f281583eacf92210c3
Yoann Dubreuil <yoann.dubreuil@gmail.com>
2019-03-16 16:39:38 +0100

    launcher fixes

--- nageru/launch-common.sh
+++ nageru/launch-common.sh
@@ -1,5 +1,5 @@
-if (( $# > 0 )); then
-    RECORDING_DIR="$(readlink -f "$1")"
+if (( $# > 1 )); then
+    RECORDING_DIR="$(readlink -f "$2")"
 else
     RECORDING_DIR="$HERE/recordings"
     mkdir -p "$RECORDING_DIR"
@@ -7,11 +7,11 @@ fi
 
 COMMON_ARGS="\
   -I. -tbreizhcamp.lua
-  --midi-mapping="$HERE/akai.mapping" \
+  --midi-mapping="$HERE/akai.midimapping" \
   --disable-alsa-output \
   --disable-gain-staging-auto \
   --disable-makeup-gain-auto \
   --mjpeg-export-cards="" \
   --recording-dir="$RECORDING_DIR" \
-  --full-screen \
+  --fullscreen \
 "
