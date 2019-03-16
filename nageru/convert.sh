62572a919f05e86185d8c84bac6953e3c5df57fe
Yoann Dubreuil <yoann.dubreuil@gmail.com>
2019-03-16 16:02:54 +0100

    clean up

--- nageru/convert.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-for i in $HOME/Vidéos/*nut; do
-  cat $i | ffmpeg -i - -acodec copy -vcodec copy $(echo ${i%%.nut}|sed 's/[:+]//g').mkv
-done
-
-mkdir -p $HOME/Vidéos/old
-mv $HOME/Vidéos/*nut $HOME/Vidéos/old
