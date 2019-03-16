f9d3d0e8ee5d263c53ad84bc4715044aca694512
Yoann Dubreuil <yoann.dubreuil@gmail.com>
2019-03-16 17:13:12 +0100

    Auto discover configuration with hostname

--- launcher/launcher.sh
+++ launcher/launcher.sh
@@ -1,7 +1,17 @@
 #!/bin/bash
 
+HERE=$(dirname "$(readlink -f "$0")")
+
+declare -A per_host_room
+per_host_room[capture-1]="Amphi D"
+per_host_room[capture-2]="Amphi C"
+per_host_room[capture-3]="Amphi B"
+
+camaaloth_id="${HOSTNAME}"
+
 java \
   -Xmx512m \
-  -jar "$(dirname "$0")/../../camaaloth-launcher/build/libs/camaaloth-launcher-0.0.1-SNAPSHOT.jar" \
-  --spring.config.location="$(dirname "$0")/application.yaml" \
+  -jar "${HERE}/../../camaaloth-launcher/build/libs/camaaloth-launcher-0.0.1-SNAPSHOT.jar" \
+  --spring.config.location="${HERE}/application.yaml" \
+  --camaaloth.breizhcamp.room="${per_host_room[$camaaloth_id]}" \
   --spring.profiles.active=breizhcamp "$@"
---
+++ nageru/launch-nageru.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+HERE=$(dirname "$(readlink -f "$0")")
+
+declare -A per_host_args
+per_host_args[capture-1]="-c 2"
+per_host_args[capture-2]="-c 4 --output-card 3 --flat-audio"
+per_host_args[capture-3]="-c 4 --output-card 3"
+
+if (( $# > 1 )); then
+    RECORDING_DIR="$(readlink -f "$2")"
+else
+    RECORDING_DIR="$HERE/recordings"
+    mkdir -p "$RECORDING_DIR"
+fi
+
+camaaloth_id="${HOSTNAME}"
+
+cd "${HERE}/themes" || exit
+nageru \
+  -I. -tbreizhcamp.lua \
+  --midi-mapping="${HERE}/mappings/akai.midimapping" \
+  --input-mapping="${HERE}/mappings/${camaaloth_id}.mapping" \
+  --disable-alsa-output \
+  --disable-gain-staging-auto \
+  --disable-makeup-gain-auto \
+  --mjpeg-export-cards="" \
+  --recording-dir="${RECORDING_DIR}" \
+  --fullscreen \
+  ${per_host_args[$camaaloth_id]}
